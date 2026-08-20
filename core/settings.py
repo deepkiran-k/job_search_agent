c045815222726b95025e1231fac63cc692f46db1
Deepkiran Kaur <deepkiran@xcrino.com>
2026-08-20 10:46:51 +0530

    updated model

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ class Settings:
             from langchain_google_genai import ChatGoogleGenerativeAI
             return ChatGoogleGenerativeAI(
                 #model="gemini-3.1-flash-lite-preview",
-                model="gemini-2.5-flash-lite",
+                model="gemini-3.5-flash-lite",
                 google_api_key=cls.GOOGLE_API_KEY,
                 temperature=0.4,
                 max_output_tokens=4096,
